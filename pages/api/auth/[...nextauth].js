34e62dd8cc052baf1a747a702f1508e378dacfa2
Noel Arzola <noel@skillcrush.com>
2022-07-24 18:56:39 -0400

    Update variable name

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,5 +8,5 @@ export default NextAuth({
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 });
